5a835559c2842140b40284aae7e4b36bcd53dfb6
wannavi <chotnt741@gmail.com>
2022-10-30 00:44:55 +0900

    deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { NextPage } from 'next';
-import { Container } from '@chakra-ui/react';
+import { Container, Flex, Text, useColorModeValue } from '@chakra-ui/react';
+
 import AssetTable from '../components/asset/AssetTable';
+import Header from '../components/shared/Header';
 
 const Index: NextPage = () => {
   return (
     <Container>
+      <Header />
       <AssetTable />
+      <Flex bg={useColorModeValue('white', 'gray.800')}>
+        <Text>Powered By Bithumb API</Text>
+      </Flex>
     </Container>
   );
 };
